84f48665f7d17a99613b67ddfb1bb5d195a188ec
Olga <baushonok@gmail.com>
2021-01-17 15:53:07 +0300

    Add page 'list'

--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -1,12 +1,22 @@
 import Head from 'next/head';
 import React from 'react';
 
+import { List } from 'components/list';
+
 const Home = () => (
     <div>
       <Head>
         <title>Experiments with Next.js</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <main>
+        <h1>
+          List of some items
+        </h1>
+
+        <List amountOfItems={2} />
+      </main>
     </div>
 );
 
